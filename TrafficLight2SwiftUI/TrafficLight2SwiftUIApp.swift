461b7023eb4b225d199a28dfb2095902753d7296
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-09 15:01:58 +0400

    Initial Commit

---
+++ TrafficLight2SwiftUI/TrafficLight2SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight2SwiftUIApp.swift
+//  TrafficLight2SwiftUI
+//
+//  Created by Elenka on 09.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight2SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
