38306a49a772591bb20d89b4156be17f9fe590d9
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-09 19:13:48 +0400

    added logic

--- TrafficLight2SwiftUI/Views/ContentView.swift
+++ TrafficLight2SwiftUI/Views/ContentView.swift
@@ -8,44 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redColorOpacity = 0.4
+    @State private var yellowColorOpacity = 0.4
+    @State private var greenColorOpacity = 0.4
+    @State private var textButton = "START"
+    
+    @State private var includedColor = 0
+    
+    
     var body: some View {
-    private var includedColor = "not included"
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
             VStack {
-                ColorCircleView(color: .red).id(1)
-                ColorCircleView(color: .yellow).id(2)
-                ColorCircleView(color: .green)
+                ColorCircleView(color: .red).opacity(redColorOpacity)
+                ColorCircleView(color: .yellow).opacity(yellowColorOpacity)
+                ColorCircleView(color: .green).opacity(greenColorOpacity)
                 Spacer()
-                Button(action: changeButtonTapped) {
-                    Text("START")}
-                        .foregroundColor(.blue)
-                        .foregroundColor(.white)
-                
+                startButton
             }
-            .padding()
+            .padding(10)
+        }
+    }
+    
+    private var startButton: some View {
+        Button(action: changeButtonTapped) {
+            Text(textButton)
+                .frame(width: 130, height: 40)
+                .cornerRadius(13)
+                .accentColor(Color.white)
+                .background(Color.blue)
+                .cornerRadius(15)
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white,lineWidth: 4))
+        }
+    }
+    
+    private func changeButtonTapped() {
+        textButton = "NEXT"
+        
+        switch includedColor {
+        case 1:
+            yellowColorOpacity = 1.0
+            redColorOpacity = 0.4
+            includedColor = 2
+        case 2:
+            greenColorOpacity = 1.0
+            yellowColorOpacity = 0.4
+            includedColor = 3
+        default:
+            redColorOpacity = 1.0
+            greenColorOpacity = 0.4
+            includedColor = 1
         }
     }
 }
-
-private func changeButtonTapped() {
-    changeButton.setTitle("NEXT", for: .normal)
-
-    switch includedColor {
-    case tag(1):
-        ColorCircleView.id(1).alpha = 1
-        redTrafficLightView.alpha = 0.3
-        includedColor = "yellow"
-    case "yellow":
-        greenTrafficLightView.alpha = 1
-        yellowTrafficLightView.alpha = 0.3
-        includedColor = "green"
-    default:
-        redTrafficLightView.alpha = 1
-        greenTrafficLightView.alpha = 0.3
-        includedColor = "red"
-    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
